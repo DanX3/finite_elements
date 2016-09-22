434ea6d3f51e7961b8982208f8808ac5db0f0364
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2016-09-22 14:30:08 +0200

    uncommented test for assemble

--- test_suite.py
+++ test_suite.py
@@ -53,7 +53,6 @@ class MeshTest(unittest.TestCase):
         self.assertItemsEqual(result, expected)
         self.assertItemsEqual(b_nodes[:5], [0,4,5,6,7])
         # assertCountEqual for python 3
-"""
 class AssembleTest(unittest.TestCase):
     def test_assemble(self):
         #topo , x , y , nodes , b_nodes = read_msh('mesh/square.msh')
@@ -70,7 +69,6 @@ class AssembleTest(unittest.TestCase):
         self.assertTrue(truth)
         #self.assertItemsEqual(result, expected)
         #self.assertItemsEqual(b_nodes[:5], [0,4,5,6,7])
-"""
 
 if __name__ == "__main__":
     unittest.main()
